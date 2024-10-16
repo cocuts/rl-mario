1a3f9203669f2d8f15bb2aa520bc1ea1e560c20f
Cory <corycutsail@gmail.com>
2024-10-15 20:46:32 -0400

    reward shaoing

--- environment/mario_env.py
+++ environment/mario_env.py
@@ -10,7 +10,10 @@ class MarioEnvironment:
         self.env = gym_super_mario_bros.make(config.ENV_NAME)
         self.env = JoypadSpace(self.env, SIMPLE_MOVEMENT)
         self.action_space = SIMPLE_MOVEMENT
-        
+        self.previous_x = 0
+        self.previous_y = 0
+        self.previous_coins = 0
+        self.jump_right_bonus = 0.5  # Bonus for jumping while moving right        
         self.last_human_action_time = time.time()
         self.ai_delay = 5 # 5 seconds delay before AI takes over
 
@@ -56,10 +59,44 @@ class MarioEnvironment:
             action_source = "Waiting for human"
         
         next_state, reward, done, info = self.env.step(action)
+        shaped_reward = self.shape_reward(info, reward, done, action)
+
         info["action_source"] = action_source
         info["action_taken"] = SIMPLE_MOVEMENT[action]
         
         return next_state, reward, done, info
+
+    def shape_reward(self, info, reward, done, action):
+        
+        # Encourage moving right
+        x_progress = info['x_pos'] - self.previous_x
+        # reward += x_progress
+        
+        # Encourage jumping while moving right
+        y_change = info['y_pos'] - self.previous_y
+        if y_change > 0 and x_progress > 0:
+            reward += self.jump_right_bonus * min(y_change, x_progress)
+        
+        # Penalize dying
+        if done and info['life'] == 0:
+            reward -= 50
+        
+        # Encourage collecting coins
+        reward += (info['coins'] - self.previous_coins) * 10
+        
+        # Encourage finishing the level
+        if info['flag_get']:
+            reward += 500
+        
+        # Additional reward for maintaining speed
+        if 'speed' in info:
+            reward += info['speed'] * 0.1
+        
+        self.previous_x = info['x_pos']
+        self.previous_y = info['y_pos']
+        self.previous_coins = info['coins']
+        
+    # Add to the __init__ method
 
     def render(self):
         self.env.render()
